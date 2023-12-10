e6e9cb526daa21eacd915214dcba095afe82d6c3
smitjiwani <smitjiwani44@gmail.com>
2023-12-10 18:19:02 +0530

    header made responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,8 @@ import { Code } from '@mui/icons-material';
 
 
 function Header() {
-    // detach header on scroll and change opacity
+
+    const [mobile, setMobile] = useState(false);
     const [show, handleShow] = useState(false);
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -25,11 +26,45 @@ function Header() {
         );
     }, []);
 
+
+    // change class when max width is 768px
+
+
+    useEffect(() => {
+        if (window.innerWidth <= 768) {
+            setMobile(true);
+        }
+        else {
+            setMobile(false);
+        }
+    }, []);
+
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth <= 768) {
+            setMobile(true);
+        }
+        else {
+            setMobile(false);
+        }
+    });
+
+
+    const [nav, setNav] = useState(false);
+
+
     return (
         <div className={`Navbar ${show && "Navbar scrolling"}`}>
-            {/* create a header file with home, about, projects, skills,  blogs */}
-            <nav className='navbar' >
-                <ul className='left'>
+
+            {mobile &&
+                <div className="nav__button">
+                    <div className="triple__dots" onClick={() => setNav(!nav)}>|||</div>
+                </div>
+            }
+
+
+            <nav className={`${mobile ? "mobile_nav" : "navbar"}`} >
+                <ul className="left">
                     <Link to="/#hero" smooth>
                         <li>
                             Home
@@ -46,7 +81,7 @@ function Header() {
                         </li>
                     </Link>
                 </ul>
-                <ul className='right'>
+                <ul className="right">
                     <a href="https://www.linkedin.com/in/smit-jiwani-1aa62816b/" target='_blank'><li><LinkedInIcon className='icon' /></li></a>
                     <a href="https://github.com/smitjiwani" target='_blank'><li><GitHubIcon className='icon' /></li></a>
                     <a href="https://codeforces.com/profile/smitttyy" target='_blank'><li><Code className='icon' /></li></a>
@@ -54,6 +89,34 @@ function Header() {
                 </ul>
             </nav>
 
+            {mobile &&
+                <div className="sidebar">
+                    <div className={`${nav ? "side__nav" : "side__nav__inactive"}`}>
+                        <div className='side__main'>
+                            <Link to="/#hero" smooth className='sm__links'>
+                                Home
+                            </Link>
+                            <Link to="/#projects" smooth className='sm__links'>
+                                Projects
+                            </Link>
+                            <Link to="/#skills" smooth className='sm__links'>
+                                Skills
+                            </Link>
+                        </div>
+
+                        <ul className='side__social'>
+                            <a href="https://www.linkedin.com/in/smit-jiwani-1aa62816b/" target='_blank'><li><LinkedInIcon className='icon' /></li></a>
+                            <a href="https://github.com/smitjiwani" target='_blank'><li><GitHubIcon className='icon' /></li></a>
+                            <a href="https://codeforces.com/profile/smitttyy" target='_blank'><li><Code className='icon' /></li></a>
+                        </ul>
+
+                        <ul className="side__contact">
+                            <a href={pdf} target='_blank'><button>Resume</button></a>
+                            <a href="mailto:smitjiwani44@gmail.com" target='_blank'><button>Contact</button></a>
+                        </ul>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
